3c597da0f45063f975f37db7a86b91b319f6fb32
GaryBian <gary.bian@gmail.com>
2018-04-18 20:24:10 -0500

    a

--- show_candle_number.py
+++ show_candle_number.py
@@ -13,3 +13,15 @@ cs.CandleStick.fromRow(hdf['TXN'].loc['2018-4-9']).describe()
 cs.CandleStick.fromRow(hdf['TSLA'].loc['2018-4-4']).describe()
 cs.CandleStick.fromRow(hdf['TSLA'].loc['2018-4-6']).describe()
 cs.CandleStick.fromRow(hdf['TSLA'].loc['2018-4-11']).describe()
+
+cs.CandleStick.fromRow(hdf['TSLA'].loc['2018-3-12']).describe()
+cs.CandleStick.fromRow(hdf['TSLA'].loc['2016-12-14']).describe()
+cs.CandleStick.fromRow(hdf['TSLA'].loc['2016-12-13']).describe()
+cs.CandleStick.fromRow(hdf['TSLA'].loc['2016-12-20']).describe()
+cs.CandleStick.fromRow(hdf['TSLA'].loc['2017-1-4']).describe()
+
+cs.CandleStick.fromRow(hdf['TSLA'].loc['2014-1-14']).describe()
+
+cs.CandleStick.fromRow(hdf['TSLA'].loc['2012-10-24']).describe()
+
+cs.CandleStick.fromRow(hdf['TSLA'].loc['2010-11-4']).describe()

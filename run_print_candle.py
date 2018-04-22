e041e1c3dbfc64bcfe7603029aaa3deae6a6d41d
GaryBian <gary.bian@gmail.com>
2018-04-22 18:15:22 -0500

    a

--- run_print_candle.py
+++ run_print_candle.py
@@ -1,10 +1,21 @@
 import toolkit
+from pandas import HDFStore
+import CandleStick as cs
+import datetime
+import talib
+import numpy
+import pandas as pd
 
 # toolkit.print_candle_debug("TSLA")
 
 # toolkit.print_candle_debug("AUMN")
 # toolkit.print_candle_debug("ECOM")
-toolkit.print_candle_debug("PODD")
-toolkit.print_candle_debug("TXN")
-toolkit.print_candle_debug("MU")
-toolkit.print_candle_debug("SQ")
+# toolkit.print_candle_debug("PODD")
+# toolkit.print_candle_debug("TXN")
+# toolkit.print_candle_debug("MU")
+# toolkit.print_candle_debug("SQ")
+
+hdf = HDFStore('data/daily.h5')
+keys = hdf.keys()
+for symbol in keys:
+    toolkit.print_candle_debug(symbol.strip('/'))

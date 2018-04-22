f8c8f98c42128b04fefac1efeb3980e24627b0da
GaryBian <gary.bian@gmail.com>
2018-04-22 13:01:48 -0500

    a

--- run_print_candle.py
+++ run_print_candle.py
@@ -2,8 +2,9 @@ import toolkit
 
 # toolkit.print_candle_debug("TSLA")
 
-#toolkit.print_candle_debug("AUMN")
-#toolkit.print_candle_debug("ECOM")
+# toolkit.print_candle_debug("AUMN")
+# toolkit.print_candle_debug("ECOM")
 toolkit.print_candle_debug("PODD")
 toolkit.print_candle_debug("TXN")
-
+toolkit.print_candle_debug("MU")
+toolkit.print_candle_debug("SQ")
--- toolkit.py
+++ toolkit.py
@@ -1,3 +1,5 @@
+from alpha_vantage.timeseries import TimeSeries
+import time
 from pandas import HDFStore
 import CandleStick as cs
 import talib
@@ -5,6 +7,8 @@ import numpy
 import pandas as pd
 
 
+# print two files, one for all candles
+# one for selected only
 def print_candle_debug(symbol):
     fulldf = HDFStore('data/daily.h5')[symbol]
     analysis = pd.DataFrame(index=fulldf.index)
@@ -15,6 +19,7 @@ def print_candle_debug(symbol):
 
     # print out candlestick debug info
     file = open(symbol + "_candle.txt", "w")
+    file_s = open(symbol + "_candle_selected.txt", "w")
     for i, row in fulldf.iterrows():
         c = cs.CandleStick.fromRow(row)
         c.associate_date(i)
@@ -24,4 +29,46 @@ def print_candle_debug(symbol):
         file.write(" | B/ATR " + "{:0.1f}".format(c.body / analysis['atr_emr'][i]))
         file.write("\n")
 
+        if yang_candle_filter(c, analysis['atr_emr'][i]):
+            file_s.write(c.describe2())
+            file_s.write(" | B/ATR " + "{:0.1f}".format(c.body / analysis['atr_emr'][i]))
+            file_s.write("\n")
+
     file.close()
+    file_s.close()
+
+
+def download_alpha_vantage(symbol):
+    # alpha_vantage's column name has index at the beginning, like "1. open"
+    # so rename it
+    symbol = symbol.strip()
+    ts = TimeSeries(key='EI7H5JUGQ20Q3GDK', output_format='pandas')
+    hdf_daily = HDFStore('data/daily.h5')
+    print(symbol)
+
+    data, meta_data = ts.get_daily_adjusted(symbol=symbol, outputsize='full')
+    data.rename(columns={'1. open': 'open',
+                         '2. high': 'high',
+                         '3. low': 'low',
+                         '4. close': 'close',
+                         '5. adjusted close': 'adjusted close',
+                         '6. volume': 'volume'
+                         }, inplace=True)
+
+    data.index = pd.to_datetime(data.index)
+    hdf_daily[symbol] = data
+    time.sleep(1)
+
+
+def yang_candle_filter(candle, atr):
+    head_to_body_ratio_max = 0.2
+    tail_to_body_ratio_max = 0.2
+    body_atr_ratio_min = 1.5
+
+    if candle.is_bull \
+            and candle.head_to_body_ratio < head_to_body_ratio_max \
+            and candle.tail_to_body_ratio < tail_to_body_ratio_max \
+            and candle.body / atr > body_atr_ratio_min:
+        return True
+
+    return False
